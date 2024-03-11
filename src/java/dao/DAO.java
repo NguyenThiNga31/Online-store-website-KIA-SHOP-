8d3819e513c9454e400a77164bb215cd2cf6d4e3
PhanHoangPhuc30 <johndoe@example.com>
2024-03-11 19:58:07 +0700

    FilterProductByPrice

--- src/java/dao/DAO.java
+++ src/java/dao/DAO.java
@@ -6,24 +6,32 @@ package dao;
 
 import context.DBContext;
 import entity.Account;
+import entity.Category;
+import entity.Product;
+import entity.SizeDetail;
+import entity.SubImage;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  *
  * @author MSI GTX
  */
 public class DAO {
-      Connection conn = null;
+
+    Connection conn = null;
     PreparedStatement ps = null;
     ResultSet rs = null;
+
     //----------------------------------------------Phúc----------------------------------------
-     public String encryptToMD5(String password) {
+    public String encryptToMD5(String password) {
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             byte[] messageDigest = md.digest(password.getBytes());
@@ -186,7 +194,8 @@ public class DAO {
         } catch (Exception e) {
         }
     }
-      // xóa tài khoản khỏi databases
+    // xóa tài khoản khỏi databases
+
     public int DeleteAccount(String email) throws Exception {
         String query = "delete from Account where email=?";
         int result = 0;
@@ -199,7 +208,8 @@ public class DAO {
         }
         return result;
     }
-     public void updateViewed() {//edit param
+
+    public void updateViewed() {//edit param
         //edit query (my_table), number of param
         String query = "update [View]\n"
                 + "set viewed = viewed + 1";
@@ -210,10 +220,152 @@ public class DAO {
         } catch (Exception e) {
         }
     }
+
+    public List<Category> getAllCategory() {
+        List<Category> list = new ArrayList<>();
+        String query = "select * from Category";
+        try {
+            conn = new DBContext().getConnection(); //mo ket noi toi sql
+            ps = conn.prepareStatement(query);//nem cau lenh query sang sql
+            rs = ps.executeQuery();//chay cau lenh query, nhan ket qua tra ve
+            while (rs.next()) {
+                list.add(new Category(rs.getInt(1), rs.getString(2)));
+            }
+
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public List<Product> getAllProduct() {
+        List<Product> list = new ArrayList<>();
+        String query = "select * from Product where isDeleted != 1";
+        DAO dao = new DAO();
+        try {
+            conn = new DBContext().getConnection(); //mo ket noi toi sql
+            ps = conn.prepareStatement(query);//nem cau lenh query sang sql
+            rs = ps.executeQuery();//chay cau lenh query, nhan ket qua tra ve
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getInt(7), dao.getAllSubImageByPID(rs.getInt(1) + ""), dao.getAllSizeByID(rs.getInt(1) + ""), rs.getInt(8), rs.getInt(9)));
+            }
+
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public List<SubImage> getAllSubImageByPID(String cid) {
+        List<SubImage> list = new ArrayList<>();
+        String query = "select S.*\n"
+                + "from Product P, SubImage S\n"
+                + "where P.pID = S.pID and S.pID = ?";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, cid);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new SubImage(rs.getInt(1), rs.getInt(2), rs.getString(3)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public List<SizeDetail> getAllSizeByID(String id) {
+        List<SizeDetail> list = new ArrayList<>();
+        String query = "SELECT *"
+                + "FROM SizeDetail SD\n"
+                + "JOIN Size S ON SD.sizeID = S.sizeID\n"
+                + "WHERE SD.pID = ?";
+        try {
+            conn = new DBContext().getConnection(); //mo ket noi toi sql
+            ps = conn.prepareStatement(query);//nem cau lenh query sang sql
+            ps.setString(1, id);
+            rs = ps.executeQuery();//chay cau lenh query, nhan ket qua tra ve
+            while (rs.next()) {
+                list.add(new SizeDetail(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getInt(4)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+    public List<Product> paginateList(List<Product> productList, int pageIndex, int numberProductPerPage) {
+        int start = (pageIndex - 1) * numberProductPerPage;
+        int end = Math.min(start + numberProductPerPage, productList.size());
+        return productList.subList(start, end);
+
+    }
+
+    public int getPageSize(int numberProduct, int allProduct) {
+        int pageSize = allProduct / numberProduct;
+        if (allProduct % numberProduct != 0) {
+            pageSize = (allProduct / numberProduct) + 1;
+        }
+        return pageSize;
+    }
+    public List<Product> getProductByPrice(double minprice, double maxprice) {
+        List<Product> list = new ArrayList<>();
+        String query = "select *\n"
+                + "from Product\n"
+                + "where price >= ? AND price <= ? AND isDeleted != 1";
+        DAO dao = new DAO();
+        try {
+            conn = new DBContext().getConnection(); //mo ket noi toi sql
+            ps = conn.prepareStatement(query);//nem cau lenh query sang sql
+            ps.setDouble(1, minprice);
+            ps.setDouble(2, maxprice);
+            rs = ps.executeQuery();//chay cau lenh query, nhan ket qua tra ve
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getInt(7), dao.getAllSubImageByPID(rs.getInt(1) + ""), dao.getAllSizeByID(rs.getInt(1) + ""), rs.getInt(8), rs.getInt(9)));
+            }
+
+        } catch (Exception e) {
+        }
+        return list;
+    }
+    public List<Product> getProductByPricecatogy(double minprice, double maxprice, String cID) {
+        List<Product> list = new ArrayList<>();
+        String query = "select *\n"
+                + "from Product\n"
+                + "where price >= ? AND price <= ? AND cID=? AND isDeleted != 1";
+        DAO dao = new DAO();
+        try {
+            conn = new DBContext().getConnection(); //mo ket noi toi sql
+            ps = conn.prepareStatement(query);//nem cau lenh query sang sql
+            ps.setDouble(1, minprice);
+            ps.setDouble(2, maxprice);
+            ps.setString(3, cID);
+            rs = ps.executeQuery();//chay cau lenh query, nhan ket qua tra ve
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getInt(7), dao.getAllSubImageByPID(rs.getInt(1) + ""), dao.getAllSizeByID(rs.getInt(1) + ""), rs.getInt(8), rs.getInt(9)));
+            }
+
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
     //------------------------------------------------------------------------------------------------
-    
-    
-       public static void main(String[] args) {
+    public static void main(String[] args) {
         DAO dao = new DAO();
     }
 
