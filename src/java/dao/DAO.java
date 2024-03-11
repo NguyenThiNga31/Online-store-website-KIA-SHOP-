b651e72b4b9c0387671efb52e3728480f78c15d5
Your Name <huybnce170981@fpt.edu.vn>
2024-03-11 09:09:37 +0700

    Update Profile

--- src/java/dao/DAO.java
+++ src/java/dao/DAO.java
@@ -216,5 +216,58 @@ public class DAO {
        public static void main(String[] args) {
         DAO dao = new DAO();
     }
+ //----------------huy---------
+    public void updateProfile(Account a) {
+        String query = "update Account\n"
+                + "set fullname = ?, email = ?, phone = ?, address= ? where uID = ?";
+        DAO dao = new DAO();
+        try {
+            conn = new DBContext().getConnection(); //mo ket noi toi sql
+            ps = conn.prepareStatement(query);//nem cau lenh query sang sql
+            ps.setString(1, a.getFullname());
+            ps.setString(2, a.getEmail());
+            ps.setString(3, a.getPhone());
+            ps.setString(4, a.getAddress());
+            ps.setInt(5, a.getId());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    public Account getAccountById(int id) {
+        String query = "SELECT * FROM Account WHERE uID = ?";
+        try {
+            ps = new DBContext().getConnection().prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Account(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getInt(7),
+                        rs.getInt(8),
+                        rs.getDate(9));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void updatePassword(int accId, String newPassword) {
+        String query = "update Account\n"
+                + "set password = ? where uID = ?";
+        try {
+            conn = new DBContext().getConnection(); //mo ket noi toi sql
+            ps = conn.prepareStatement(query);//nem cau lenh query sang sql
+            ps.setString(1, newPassword);
+            ps.setInt(2, accId);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+    }
 }
